b6cc4bdbe9d41b9e7634de59afc2bdc754566f55
Sandra-Duarte <106591234+Sandra-Duarte@users.noreply.github.com>
2023-05-23 17:37:35 +0100

    update home style

--- src/Componenent/Page/Home/Home.jsx
+++ src/Componenent/Page/Home/Home.jsx
@@ -27,18 +27,17 @@ const Home = () => {
            <span className='introText'> Hi my name is,</span>
            <h1 className='title'> Sandra Duarte </h1>
            <span className='subTitle'>I Front-end develop</span>
-           <p>Sign up & login – after verification go to signup page  and also in login details put wrong data( those r not
-              in user_data table & later input any data from the table to make sure that our functionality works)
+           <p className='homeParagraph'>Sign up & login – after verification go to signup page  and also in login details put wrong data( those r not in user_data table & later input any data from the table to make sure that our functionality works)
               </p>
 
            <div className='lowerHomeSection'>
             <button className='contactBtn'>
-              <a href='#contact' className='flex'> Contact Me <TbArrowBigRightLines className ="iconB"/></a>
+              <a href='#contact' className='flex'> Contact Me <TbArrowBigRightLines className ="icon"/></a>
             </button>
             <div className='scrollDiv'>
             <a href='#about' className='flex'>
             <h6 className='scroll'>Scroll Down</h6>
-            <FiChevronsDown className ="iconD"/>
+            <FiChevronsDown className ="icon"/>
             </a>
             </div>
             </div>
