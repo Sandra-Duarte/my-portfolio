653070ade595c60633755f1ea13662c638ded677
Sandra-Duarte <106591234+Sandra-Duarte@users.noreply.github.com>
2023-05-26 21:20:47 +0100

    creacte the projects page

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import Navbar from './Componenent/Page/Navbar/Navbar'
 import Home from './Componenent/Page/Home/Home'
 import Skills from './Componenent/Page/Skills/Skills'
 import About from './Componenent/Page/About/About'
-// import Skills from "./Componenent/Page/Skills/Skills"
-// import Project from './Componenent/Page/Project/Project'
+import Project from './Componenent/Page/Project/Project'
 // import Footer from './Componenent/Page/Footer.js/Footer'
 // import Contact from './Componenent/Page/Contact/Contact'
 
@@ -16,8 +15,8 @@ const App = () => {
     <Home />
    <About/>
     <Skills/>
-    {/* <Project/>
-    <Contact/>
+    <Project/>
+    {/* <Contact/>
     <Footer/> */}
 
     </>
---
+++ src/Componenent/Page/Project/Project.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Project.scss"
+
+const Project = () => {
+  return (
+    <section id="projects" className='projects container sections'>
+     <div className='sectionTitle'>
+  <span className='titleNumber'> 03.</span>
+  <h5 className='titleText'> Projects
+  <div className='underline'><span></span></div>
+  </h5>
+
+ </div>
+    </section>
+  )
+}
+
+export default Project
