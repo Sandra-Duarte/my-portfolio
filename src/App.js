e0eb54256a85e3c2d54b775838e8e2466e25a473
Sandra-Duarte <106591234+Sandra-Duarte@users.noreply.github.com>
2023-06-10 23:53:27 +0100

    erro fixe

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import "./App.css"
 import Navbar from './Componenent/Page/Navbar/Navbar'
-import Home from './Componenent/Page/Home/Home'
-import Skills from './Componenent/Page/Skills/Skills'
-import About from './Componenent/Page/About/About'
-import Project from './Componenent/Page/Project/Project'
+import Home from './Componenent/Page/Home Section/Home'
+import Skills from './Componenent/Page/Skills Section/Skills'
+import About from './Componenent/Page/About Section/About'
+import Project from './Componenent/Page/Project Section/Project'
 // import Footer from './Componenent/Page/Footer.js/Footer'
-import Contact from './Componenent/Page/Contact/Contact'
+import Contact from './Componenent/Page/Contact Section/Contact'
 
 const App = () => {
   return (
